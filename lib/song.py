372ee8a1687feec2f3f8bd66e0c54f22f226b70b
daudikombe <daudikombe77@gmail.com>
2026-06-14 00:11:37 +0300

    feat:use init runs every time you create a song

--- lib/song.py
+++ lib/song.py
@@ -4,5 +4,18 @@ class Song:
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
+    artists_count ={}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Call all class methods when a new song is created
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
+
    
